0251ff59e4c11e00723c7685ca2abdf1b1e3dc8d
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-08-28 12:01:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    switch(n) {
+        case 1: cout << "One"; break;
+        case 2: cout << "Two"; break;
+        case 3: cout << "Three"; break;
+        case 4: cout << "Four"; break;
+        case 5: cout << "Five"; break;
+        case 6: cout << "Six"; break;
+        case 7: cout << "Seven"; break;
+        case 8: cout << "Eight"; break;
+        case 9: cout << "Nine"; break;
+        default: cout << "Unknown";
+    }
+
+    return 0;
+}
