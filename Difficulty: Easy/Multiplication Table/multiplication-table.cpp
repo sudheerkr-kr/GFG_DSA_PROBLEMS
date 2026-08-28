1cc1aaaa522f4643720190403418d96459480f1d
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-08-28 12:12:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Multiplication Table/multiplication-table.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans=1;
+    int i =1;
+    // code here
+    for(i ; i<=10 ; i++){
+        ans= n*i;
+        cout<<ans<<" ";
+        
+    }
+
+    return 0;
+}
